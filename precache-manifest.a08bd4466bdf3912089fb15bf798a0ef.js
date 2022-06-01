10c7f9c35d30f67ddccb87abce7450270b8c53af
arshad <arshadkperambra1@gmail.com>
2022-06-02 01:53:46 +0530

    Updates

---
+++ precache-manifest.a08bd4466bdf3912089fb15bf798a0ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8eeae54a0cc780a5e8cf",
+    "url": "/todo-app/static/css/main.15cb47e9.chunk.css"
+  },
+  {
+    "revision": "8eeae54a0cc780a5e8cf",
+    "url": "/todo-app/static/js/main.8eeae54a.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/todo-app/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "c1208bc16b3facea6fea",
+    "url": "/todo-app/static/js/2.c1208bc1.chunk.js"
+  },
+  {
+    "revision": "f5ba316e945ba0f394eb",
+    "url": "/todo-app/static/js/runtime~main.f5ba316e.js"
+  },
+  {
+    "revision": "0cf87a5a5796686eb7ff3c486e8e1981",
+    "url": "/todo-app/index.html"
+  }
+];

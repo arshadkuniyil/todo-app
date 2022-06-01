a4a7046dabea4659c22248cab13859ac84000153
ARSHAD <arshadkperambra@gmail.com>
2022-06-01 22:40:10 +0530

    seperated deleted todos

--- src/App.js
+++ src/App.js
@@ -8,82 +8,136 @@ function App() {
 
   const [toDo, setTodo] = useState('')
   const [toDos, setTodos] = useState([])
+  const [dltToDos, setDltTodos] = useState([])
 
 
 
   return (
     <div className="app">
-
-      <div className="mainHeading">
-        <h1>ToDo List</h1>
-      </div>
-
-      <div className="subHeading">
-        <br />
-        <h2>Whoop, it's {dayName} 🌝 ☕ </h2>
-      </div>
-
-      <div className="input">
-        <input value={toDo}
-          onChange={(e) => {
-            setTodo(e.target.value)
-          }}
-
-          type="text" placeholder="🖊️ Add item..." />
-
-        <i className="fas fa-plus" onClick={
-          () => {
-            setTodo('')
-            setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])
-          }
-        }
-        > </i>
-      </div>
-
-      <div className="todos">
+      <div className='deleted_todos'>
+        <h1>Deleted</h1>
         {
-          toDos.map((obj, index) => {
-
-
-            if (obj.status) {
-              return null
-            }
+          dltToDos.map((obj, index) => {
+            
             return (
               <div className="todo" key={index}>
 
                 <div className="left">
 
-                  <input type="checkbox" name="" id="" checked={obj.status}
-                    onChange={(e) => {
+                  <i className="fas fa-redo" onClick={() => {
+                    setDltTodos(dltToDos.filter(obj2 => (obj2.id !== obj.id)))
 
-                      setTodos(toDos.filter(obj2 => {
-                        if (obj2.id === obj.id) {
-                          obj2.status = e.target.checked
-                        }
-                        return obj2
-                      }))
-                    }} />
+                    dltToDos.filter(obj2 => {
+                      if (obj2.id === obj.id) setTodos([...toDos, obj2])
+                    })
+
+                  }} />
 
                   <p>{obj.text} {"\n"} {new Date(obj.id).toLocaleString()}</p>
 
                 </div>
 
                 <div className="right">
-                  <i className="fas fa-times"
+                  <i className="fas fa-times" style={{"color":"red"}}
                     onClick={
                       () => {
-                        const newTodos = toDos.filter((obj2) => obj2.id !== obj.id);
-                        setTodos(newTodos);
+                        const newTodos = dltToDos.filter(obj2 => obj2.id !== obj.id);
+                        setDltTodos(newTodos);
+
                       }
-                    }></i>
+                    }
+                  ></i>
                 </div>
 
               </div>
             )
-           })
-          }
+          })
+        }
 
-{
+      </div>
+
+      <div className='main'>
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's {dayName} 🌝 ☕ </h2>
+        </div>
+
+        <div className="input">
+          <input value={toDo}
+            onChange={(e) => {
+              setTodo(e.target.value)
+            }}
+
+            type="text" placeholder="🖊️ Add item..." />
+
+          <i className="fas fa-plus" onClick={
+            () => {
+              setTodo('')
+              setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])
+            }
+          }
+          > </i>
+        </div>
+
+        <div className="todos">
+
+          {
+            toDos.map((obj, index) => {
+
+
+              if (obj.status) {
+                return null
+              }
+              return (
+                <div className="todo" key={index}>
+
+                  <div className="left">
+
+                    <input type="checkbox" name="" id="" checked={obj.status}
+                      onChange={(e) => {
+
+                        setTodos(toDos.filter(obj2 => {
+                          if (obj2.id === obj.id) {
+                            obj2.status = e.target.checked
+                          }
+                          return obj2
+                        }))
+                      }} />
+
+                    <p>{obj.text} {"\n"} {new Date(obj.id).toLocaleString()}</p>
+
+                  </div>
+
+                  <div className="right">
+                    <i className="fas fa-times"
+                      onClick={
+                        () => {
+                          const newTodos = toDos.filter(obj2 => obj2.id !== obj.id);
+                          setTodos(newTodos);
+                          toDos.filter(obj2 => {
+                            if (obj2.id === obj.id) setDltTodos([...dltToDos, obj2])
+                          })
+                        }
+                      }
+                    ></i>
+                  </div>
+
+                </div>
+              )
+            })
+          }
+        </div>
+
+
+      </div>
+
+      <div className='completed'>
+        <h1 > Completed</h1>
+        {
           toDos.map((obj, index) => {
 
 
@@ -91,8 +145,8 @@ function App() {
               return null
             }
             return (
-              
-              <div className="todo" key={index}>
+
+              <div className="todo_completed" key={index}>
                 <div className="left">
 
                   <input type="checkbox" name="" id="" checked={obj.status}
@@ -114,20 +168,21 @@ function App() {
                   <i className="fas fa-times"
                     onClick={
                       () => {
-                        const newTodos = toDos.filter((obj2) => obj2.id !== obj.id);
+                        const newTodos = toDos.filter(obj2 => obj2.id !== obj.id);
                         setTodos(newTodos);
+                        toDos.filter(obj2 => {
+                          if (obj2.id === obj.id) setDltTodos([...dltToDos, obj2])
+                        })
                       }
                     }></i>
                 </div>
 
               </div>
             )
-           })
-          }
-
-
-
+          })
+        }
       </div>
+
 
     </div>
   );
